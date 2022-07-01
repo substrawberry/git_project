2fe1a4df984dc656d269b9d618ee8e446edc7d4d
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-01 22:25:11 +0900

    first submit
    first submit

--- unity_github/Assets/Script/TestScript5.cs
+++ unity_github/Assets/Script/TestScript5.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBranch : MonoBehaviour
+public class TestScript5 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
