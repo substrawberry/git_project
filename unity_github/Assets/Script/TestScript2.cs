b408e6d58d560077d5df3d3452c64d9072367042
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-03 17:18:27 +0900

    desktop commit
    commit~~

--- unity_github/Assets/Script/TestScript2.cs
+++ unity_github/Assets/Script/TestScript2.cs
@@ -7,7 +7,7 @@ public class TestScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test 2
     }
 
     // Update is called once per frame
