95ebdbbbd5cb520b9e2462b6bcf6aca75b2bb61b
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-04 22:02:06 +0900

    commit test
    commit!
    amend

--- unity_github/Assets/Script/TestScript1.cs
+++ unity_github/Assets/Script/TestScript1.cs
@@ -7,7 +7,7 @@ public class TestScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 1 : 3 submit");
+        Debug.Log("TestScript 1 : 4 submit");
     }
 
     // Update is called once per frame
