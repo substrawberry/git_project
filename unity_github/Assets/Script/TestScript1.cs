b188e3f8e98994b066e590113a8b673ee10e7e5c
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-05 01:17:47 +0900

    push origin
    push!! + amend

--- unity_github/Assets/Script/TestScript1.cs
+++ unity_github/Assets/Script/TestScript1.cs
@@ -7,7 +7,7 @@ public class TestScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 1 : 4 submit");
+        Debug.Log("TestScript 1 : 5 push test");
     }
 
     // Update is called once per frame
