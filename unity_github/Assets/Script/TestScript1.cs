10069aac02d7334f3bc09e3035c3f0a2961667ca
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-05 20:07:46 +0900

    aaa
    aaa

--- unity_github/Assets/Script/TestScript1.cs
+++ unity_github/Assets/Script/TestScript1.cs
@@ -7,7 +7,7 @@ public class TestScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 1 : 5 main merge test");
+        Debug.Log("TestScript 1 : 6 main merge test");
     }
 
     // Update is called once per frame
