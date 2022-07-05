8bc29f5481eb18d4e522db547f24d2e399314f36
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-05 23:32:33 +0900

    version 0.0
    init

--- unity_github/Assets/Script/TestScript4.cs
+++ unity_github/Assets/Script/TestScript4.cs
@@ -7,7 +7,7 @@ public class TestScript4 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 4 : 3 submit");
+        Debug.Log("TestScript 4");
     }
 
     // Update is called once per frame
--- unity_github/Assets/Script/TestScript5.cs
+++ unity_github/Assets/Script/TestScript5.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class TestScript5 : MonoBehaviour
 {
-    int getBloodStrawberry()
-    {
-        return 12345;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 5 : 4 submit");
+        Debug.Log("TestScript 5");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Diff Test
-        Debug.Log("bloodstrawberry!!");
+
     }
 }
