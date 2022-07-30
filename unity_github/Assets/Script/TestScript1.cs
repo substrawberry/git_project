a029141c83095aa8459fd0a3b072218fac1239c2
substrawberry <40860674+bloodstrawberry@users.noreply.github.com>
2022-07-30 19:58:36 +0900

    Revert "ver 1.0"
    This reverts commit c348768c85aea4c35e63b6dc96730713612e1556.

--- unity_github/Assets/Script/TestScript1.cs
+++ unity_github/Assets/Script/TestScript1.cs
@@ -7,7 +7,7 @@ public class TestScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestScript 1 - first");
+        Debug.Log("TestScript 1");
     }
 
     // Update is called once per frame
